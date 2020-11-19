bc5c1e648129d80dc548887081350502cb20cb2c
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2020-11-19 14:28:39 -0500

    Update access token endpoint to match existing sample backend

--- functions/access-token.js
+++ functions/access-token.js
@@ -7,15 +7,15 @@
  *
  *  Parameters
  *  - identity - required - unique user id, no PII
- * 
+ *
  *
  *  Returns JSON
- * 
+ *
  *  on Success:
  *  {
  *    "token": "eyJ6aXAiOiJERUYiLCJraWQiOiJTQVNfUzNfX19...."
  *  }
- * 
+ *
  *  on Error:
  *  {
  *    "error" {
@@ -24,48 +24,57 @@
  *    }
  *  }
  */
+var crypto = require("crypto");
+
+function generateSHA256(input) {
+  return crypto.createHash("sha256").update(input).digest("hex");
+}
 
 exports.handler = function (context, event, callback) {
   const response = new Twilio.Response();
-  response.appendHeader('Content-Type', 'application/json');
+  response.appendHeader("Content-Type", "application/json");
 
   // uncomment to support CORS
   // response.appendHeader('Access-Control-Allow-Origin', '*');
   // response.appendHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   // response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (typeof event.identity === 'undefined') {
+  if (typeof event.identity === "undefined") {
     response.setBody({
-      "error": {
-        "message": "Missing parameter; please provide a unique user identity.",
-        "moreInfo": "https://www.twilio.com/docs/verify/api/access-token"
-      }
-    })
+      error: {
+        message: "Missing parameter; please provide a unique user identity.",
+        moreInfo: "https://www.twilio.com/docs/verify/api/access-token",
+      },
+    });
     response.setStatusCode(400);
     return callback(null, response);
   }
 
   const client = context.getTwilioClient();
-  const service = context.VERIFY_SERVICE_SID;
+  const serviceSid = context.VERIFY_SERVICE_SID;
+  const identity = context.HASH_IDENTITY
+    ? generateSHA256(event.identity)
+    : event.identity;
+  const factorType = "push";
 
-  client.verify.services(service)
-    .accessTokens
-    .create({ identity: event.identity, factorType: 'push' })
-    .then(resp => {
-      response.setStatusCode(200)
+  client.verify
+    .services(serviceSid)
+    .accessTokens.create({ identity, factorType })
+    .then((token) => {
+      response.setStatusCode(200);
       response.setBody({
-        "token": resp.token
-      })
+        ...token,
+        identity,
+        serviceSid,
+        factorType,
+      });
       callback(null, response);
-    }).catch(error => {
+    })
+    .catch((error) => {
       console.log(error);
       response.setStatusCode(error.status);
       response.setBody({
-        "success": false,
-        "error": {
-          "message": error.message,
-          "moreInfo": error.moreInfo
-        }
+        ...error,
       });
       callback(null, response);
     });
---
+++ functions/create-challenge.js
@@ -0,0 +1,73 @@
+/**
+ *  Requeset an Access Token
+ *
+ *
+ *  Pre-requisites
+ *  - Create a Verify Service (https://www.twilio.com/console/verify/services)
+ *
+ *  Parameters
+ *  - entity - required
+ *  - factor_sid - required
+ * 
+ *
+ *  Returns JSON
+ * 
+ *  on Success:
+ *  challenge JSON - https://www.twilio.com/docs/verify/api/challenge#challenge-properties
+ * 
+ *  on Error:
+ *  {
+ *    "error" {
+ *       "message": "Details about your error",
+ *       "moreInfo": "Link to error"
+ *    }
+ *  }
+ */
+
+exports.handler = function (context, event, callback) {
+  const response = new Twilio.Response();
+  response.appendHeader('Content-Type', 'application/json');
+
+  // uncomment to support CORS
+  // response.appendHeader('Access-Control-Allow-Origin', '*');
+  // response.appendHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  // response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (typeof event.entity === 'undefined') {
+    response.setBody({
+      "error": {
+        "message": "Missing parameter; please provide an entity.",
+        "moreInfo": "https://www.twilio.com/docs/verify/api/challenge#create-a-challenge-resource"
+      }
+    })
+    response.setStatusCode(400);
+    return callback(null, response);
+  }
+
+  const client = context.getTwilioClient();
+  const service = context.VERIFY_SERVICE_SID;
+
+  client.verify
+    .services(service)
+    .entities(entity)
+    .challenges.create({
+      factorSid: factor_sid,
+      'details.message': message,
+      'details.fields': fields
+    }).then(challenge => {
+      response.setStatusCode(200)
+      response.setBody(challenge)
+      callback(null, response);
+    }).catch(error => {
+      console.log(error);
+      response.setStatusCode(error.status);
+      response.setBody({
+        "success": false,
+        "error": {
+          "message": error.message,
+          "moreInfo": error.moreInfo
+        }
+      });
+      callback(null, response);
+    });
+};
