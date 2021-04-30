537480c33ddf652ed873ef50647f00dcccbae18b
Kelley Robinson <3673341+robinske@users.noreply.github.com>
2021-04-30 16:04:12 -0400

    Remove factor from showing up in details

--- functions/create-challenge.js
+++ functions/create-challenge.js
@@ -10,6 +10,7 @@
  *
  *  Parameters
  *  - identity - required
+ *  - factor SID - required
  *  - message - required
  *  - details like IP, Location, etc. - optional
  *
@@ -54,12 +55,12 @@ exports.handler = function (context, event, callback) {
 
   requiredParameter(event.identity, "identity", response, callback);
   requiredParameter(event.message, "message", response, callback);
-  requiredParameter(event.message, "factor", response, callback);
+  requiredParameter(event.factor, "factor", response, callback);
 
   const client = context.getTwilioClient();
   const service = context.VERIFY_SERVICE_SID;
 
-  let { identity, message, ...details } = event;
+  let { identity, message, factor, ...details } = event;
   let fields = [];
   for (const [key, value] of Object.entries(details)) {
     fields.push({ label: key, value: value });
@@ -68,43 +69,72 @@ exports.handler = function (context, event, callback) {
   client.verify
     .services(service)
     .entities(identity)
-    .factors.list({ limit: 20 })
-    .then((factors) => {
-      if (factors.length === 0) {
-        response.setBody({
-          error: {
-            message:
-              "No factors found for identity. Register a factor before continuing.",
-            moreInfo: "https://www.twilio.com/docs/verify/api/factor",
-          },
-        });
-        callback(null, response);
-      }
-      factors.forEach(({ sid }) =>
-        client.verify
-          .services(service)
-          .entities(identity)
-          .challenges.create({
-            factorSid: sid,
-            "details.message": message,
-            "details.fields": fields,
-          })
-          .then((challenge) => {
-            response.setStatusCode(200);
-            response.setBody(challenge);
-            callback(null, response);
-          })
-          .catch((error) => {
-            console.log(error);
-            response.setStatusCode(error.status);
-            response.setBody({
-              error: {
-                message: error.message,
-                moreInfo: error.moreInfo,
-              },
-            });
-            callback(null, response);
-          })
-      );
+    .challenges.create({
+      factorSid: event.factor,
+      "details.message": message,
+      "details.fields": fields,
+    })
+    .then((challenge) => {
+      response.setStatusCode(200);
+      response.setBody(challenge);
+      callback(null, response);
+    })
+    .catch((error) => {
+      console.log(error);
+      response.setStatusCode(error.status);
+      response.setBody({
+        error: {
+          message: error.message,
+          moreInfo: error.moreInfo,
+        },
+      });
+      callback(null, response);
     });
+
+  // client.verify
+  // .services(service)
+  // .entities(identity)
+  // .factors.list({ limit: 20 })
+  // .then((factors) => {
+  //   if (factors.length === 0) {
+  //     response.setBody({
+  //       error: {
+  //         message:
+  //           "No factors found for identity. Register a factor before continuing.",
+  //         moreInfo: "https://www.twilio.com/docs/verify/api/factor",
+  //       },
+  //     });
+  //     callback(null, response);
+  //   }
+  //
+  //   factors.forEach(({ sid }) =>
+  // const challenge = {
+  //   factorSid: event.factor,
+  //   "details.message": message,
+  // };
+
+  // if (fields.length > 0) challenge["details.fields"] = fields;
+
+  //     client.verify
+  //       .services(service)
+  //       .entities(identity)
+  //       .challenges.create(challenge)
+  //       .then((c) => {
+  //         response.setStatusCode(200);
+  //         response.setBody(c);
+  //         callback(null, response);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //         response.setStatusCode(error.status);
+  //         response.setBody({
+  //           error: {
+  //             message: error.message,
+  //             moreInfo: error.moreInfo,
+  //           },
+  //         });
+  //         callback(null, response);
+  //       })
+  //   );
+  // });
 };
